38953622b07fef207536a7d876351dc6f033d237
Alessandro Chumpitaz <par_214@hotmail.com>
2024-01-04 14:28:41 -0500

    :test_tube: add bank tests

---
+++ spec/models/bank_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+
+RSpec.describe Bank, type: :model do
+  describe 'validations' do
+    it { should validate_length_of(:name).is_at_least(3).is_at_most(50) }
+  end
+end
