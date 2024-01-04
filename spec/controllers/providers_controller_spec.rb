382e84e1d6ee3f9d36a0833a2f767e933c1657ac
Alessandro Chumpitaz <par_214@hotmail.com>
2024-01-04 17:18:18 -0500

    :test_tube: add index text for providers controller

--- spec/controllers/providers_controller_spec.rb
+++ spec/controllers/providers_controller_spec.rb
@@ -3,6 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe ProvidersController, type: :controller do
+  describe 'GET #index' do
+    it 'renders the index template' do
+      get :index
+
+      expect(response).to render_template('index')
+    end
+  end
+
   describe 'GET #new' do
     it 'renders the new template' do
       get :new
